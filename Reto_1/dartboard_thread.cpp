f923737abab8fd1906fe991c422f69574afc4f55
Miguel <migue.marulanda99@gmail.com>
2020-10-06 01:06:33 -0500

    Hilos Optimos

--- Reto_1/dartboard_thread.cpp
+++ Reto_1/dartboard_thread.cpp
@@ -15,33 +15,41 @@
 
 using namespace std; 
 
-void lanzar(double &n, double &p, double &factor){
+double* p;
+double* x;
+double* y;
+
+void lanzar(double &n, long id){
 	
 	long i; 
-	double num_per_thr = n/NUM_OF_THREADS;
-	//double start_index = id * num_per_thr; 
-	//double final_index = start_index + num_per_thr;
-	double x; 
-	double y; 
-
-	for(i = 0; i < num_per_thr; i++){
-		x = (double)rand()*factor;       // random number (0 - 1)  
-		y = (double)rand()*factor;       // random number (0 - 1)
-        if (x*x + y*y < 1.0){
-            p++;                                    
+	long num_per_thr = n/NUM_OF_THREADS;
+	long start_index = id * num_per_thr; 
+	long final_index = start_index + num_per_thr;
+	
+	for(i = start_index; i < final_index; i++){
+        if (x[i]*x[i] + y[i]*y[i] < 1.0){
+            p[id]++;                                    
         }
+	}
+}
+
+void gendata(double n, double factor) {
+	for(long i = 0; i < n; i++){
+	x[i] = (double)rand()*factor;       // random number (0 - 1)
+	y[i] = (double)rand()*factor;       // random number (0 - 1)
 	}
 }
 
 int main(int argc, char* argv[]){
 	//int n = (int)atoi(argv[1]);
 
-    double x; // x coordinate of the dart
-    double y; // y coordinate of the dart
+    //double x; // x coordinate of the dart
+	//double y; // y coordinate of the dart
     double factor; // limit of rand function
     double n; // amount of trials
-    double p; // positive trials
+    //double p; // positive trials
     double pi; // output
+	double aux;
     //double i; // loop counter
 
 	srand(time(NULL));
@@ -50,6 +58,10 @@ int main(int argc, char* argv[]){
 	p = 0;
 	factor = 1.0 / RAND_MAX;
 
+	p = new double [(long)NUM_OF_THREADS];
+	x = new double [(long)n];
+	y = new double [(long)n];
+
 	struct timeval start;
 	struct timeval end;
 	double milisecs;
@@ -57,20 +69,28 @@ int main(int argc, char* argv[]){
 
     vector<thread> threads; //creacion de n hilos
 
+	gendata(n, factor);
+
 	gettimeofday(&start, 0);
 	for (long i=0; i<NUM_OF_THREADS; i++){
 		threads.push_back(thread(
-			lanzar, ref(n), ref(p), ref(factor)
+			lanzar, ref(n), i
 		));
 	}
     for (thread &t : threads)
         t.join();
 	gettimeofday(&end, 0);
 
+	for(long i = 0; i < NUM_OF_THREADS; i++) {
+		aux+= p[i];
+	}
+
 	seconds  = end.tv_sec  - start.tv_sec;
 	useconds = end.tv_usec - start.tv_usec;
 	milisecs = ((seconds) * 1000 + useconds/1000.0);
-	cout << n << "\t" << p <<"\t"; 
-	pi = 4*p/n;
+	cout << n << "\t" << aux <<"\t"; 
+	
+	pi = 4*aux/n;
+	
 	cout << milisecs << "\t" << pi << endl;
 }
--- Reto_1/needle_thread.cpp
+++ Reto_1/needle_thread.cpp
@@ -15,37 +15,50 @@
 
 using namespace std; 
 
-void lanzar(double &n, double &l,  double &p, double &PI){
+double* p;
+double* x;
+double* y;
+
+void lanzar(double &n,  long id){
 	
 	long i; 
-	double num_per_thr = n/NUM_OF_THREADS;
-	//double start_index = id * num_per_thr; 
-	//double final_index = start_index + num_per_thr;
-	double x; 
-	double y; 
-
-	for(i = 0; i < num_per_thr; i++){
-		//double k = (double)rand()/(RAND_MAX)*2*PI;      // random angle (0 - 2PI)
-		 x = (double)rand()/(RAND_MAX)/2;         		  // random x (0 - 0.5)
-		 y = (l/2) * sin ((double)rand()/(RAND_MAX)*2*PI);
-		if (x <= y){
-			p++;              
+	long num_per_thr = n/NUM_OF_THREADS;
+	long start_index = id * num_per_thr; 
+	long final_index = start_index + num_per_thr;
+	
+	for(i = start_index; i < final_index; i++){
+		if (x[i] <= y[i]){
+			p[id]++;
 		}
+	}
+}
+
+void gendata(double n, double l, double PI){
+for(long i = 0; i < n; i++){
+		//double k = (double)rand()/(RAND_MAX)*2*PI;      // random angle (0 - 2PI)
+		 x[i] = (double)rand()/(RAND_MAX)/2;         		  // random x (0 - 0.5)
+		 y[i] = (l/2) * sin ((double)rand()/(RAND_MAX)*2*PI);
 	}
 }
 
 int main(int argc, char* argv[]){
 
 	double PI; // PI Real Value
-	double p; // positive trials per thread
+	//double p; // positive trials per thread
 	double pi; // output
 	double l; // lenght of the needle
 	double n; // amount of trials
+	double aux;
+	
+
 
 	srand(time(NULL));
 	n = (double)atoi(argv[1]);
 	//n = 10000; //cantidad de pruebas
 	l = 1; 
+	p = new double [(long)NUM_OF_THREADS];
+	x = new double [(long)n];
+	y = new double [(long)n];
 
 	PI = acos(-1.0); 
 
@@ -55,11 +68,13 @@ int main(int argc, char* argv[]){
 	struct timeval end;
 	double milisecs;
 	long seconds, useconds;
+	
+	gendata(n,l, PI);
 
 	gettimeofday(&start, 0);
 	for(long i = 0; i < NUM_OF_THREADS; i++) {
 		threads.push_back(thread(
-			lanzar, ref(n), ref(l), ref(p), ref(PI)
+			lanzar, ref(n), i
 		));
 	}
 	
@@ -67,12 +82,16 @@ int main(int argc, char* argv[]){
         t.join();
 	gettimeofday(&end, 0);
 
+	for(long i = 0; i < NUM_OF_THREADS; i++) {
+		aux+= p[i];
+	}
+
 	seconds  = end.tv_sec  - start.tv_sec;
 	useconds = end.tv_usec - start.tv_usec;
 	milisecs = ((seconds) * 1000 + useconds/1000.0);
-	cout << n << "\t" << p <<"\t"; 
+	cout << n << "\t" << aux <<"\t"; 
 
-	pi = (l/p)*(n);
+	pi = (l/aux)*(n);
 
 	cout << milisecs << "\t" << pi << endl;
 	pthread_exit(NULL);
