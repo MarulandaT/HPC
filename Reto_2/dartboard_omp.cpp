193d3dfe4a4bbdf041e5898b32118e8cc9c27acd
MigueLinux <migue.marulanda99@gmail.com>
2020-10-29 10:16:53 -0500

    reto2 omp

--- Reto_2/dartboard_omp.cpp
+++ Reto_2/dartboard_omp.cpp
@@ -9,9 +9,9 @@
 #include <math.h>
 #include <omp.h>
 #define NUM_OF_THREADS 4
-//g++ -o ${args[3]} -fopenmp ${args[3]}.cpp 
+//g++ -o dartboard_omp -fopenmp dartboard_omp.cpp
 
-using namespace std; 
+using namespace std;  
 
 void lanzar(double n, double *p, double *x, double *y){
 	
@@ -19,68 +19,64 @@ void lanzar(double n, double *p, double *x, double *y){
 	long num_per_thr;
 	long start_index; 
 	long final_index;
-
-	#pragma omp parallel firstprivate(p,x,y), private(i, num_per_thr, start_index, final_index), num_threads(NUM_OF_THREADS)
+	
+	#pragma omp parallel firstprivate(p,x,y), private (i, num_per_thr, start_index, final_index), num_threads(NUM_OF_THREADS)
 	{
 	num_per_thr = n / omp_get_num_threads();
 	start_index = omp_get_thread_num() * num_per_thr; 
 	final_index = start_index + num_per_thr;
-	
+
 	for(i = start_index; i < final_index; i++){
-		if (x[i] <= y[i]){
-			p[omp_get_thread_num()]++;
-		}
+        if (x[i]*x[i] + y[i]*y[i] < 1.0){
+            p[omp_get_thread_num()]++;                                    
+        }
 	}
 	}
 }
 
-void gendata(double n, double l, double PI, double *x, double *y){
+void gendata(double n, double factor, double *x, double *y) {
 	for(long i = 0; i < n; i++){
-	x[i] = (double)rand()/(RAND_MAX)/2;         		  // random x (0 - 0.5)
-	y[i] = (l/2) * sin ((double)rand()/(RAND_MAX)*2*PI);
+	x[i] = (double)rand()*factor;       // random number (0 - 1)
+	y[i] = (double)rand()*factor;       // random number (0 - 1)
 	}
 }
 
 int main(int argc, char* argv[]){
-
-	double PI; // PI Real Value
-	double pi; // output
-	double l; // lenght of the needle
-	double n; // amount of trials
+	
+    double factor; // limit of rand function
+    double n; // amount of trials
+    double pi; // output
 	double aux; //Aciertos de todos los threads
 	double *p; //Aciertos 
-	double *x; //Posición de la aguja 
-	double *y; //Angulo con respecto a los ejes
-	
+	double *x; //Posición del dardo en x
+	double *y; //Posición del dardo el y 
+
 	srand(time(NULL));
 
 	n = (double)atoi(argv[1]);
-	l = 1; //Tamaño de la aguja 
+	factor = 1.0 / RAND_MAX; //Factor para numeros aleatorios
 
-	//Reserva de memoria
-	p = new double [NUM_OF_THREADS];
+	//Reserva de Memoria 
+	p = new double [(long)NUM_OF_THREADS];
 	x = new double [(long)n];
 	y = new double [(long)n];
 
-	//Calculo del valor real de PI según math.h
-	PI = acos(-1.0); 
-
-	//Variables para calcular los tiempos 
+	//Variables para calcular los tiempos.
 	struct timeval start;
 	struct timeval end;
 	double milisecs;
 	long seconds, useconds;
-	
-	//Generar un conjunto de datos para los lanzamientos 
-	gendata(n,l, PI, x, y);
+
+	//Generar un conjunto de datos para los lanzamientos
+	gendata(n, factor, x, y);
 
 	gettimeofday(&start, 0);
 	//Hacer los lanzamientos
 	lanzar(n, p, x, y);
 	gettimeofday(&end, 0);
 
-	//Acumular la cantidad de acierdos de cada thread
-	for(long i = 0; i <NUM_OF_THREADS; i++) {
+	//Acumular la cantidad de aciertos en cada thread
+	for(long i = 0; i < NUM_OF_THREADS; i++) {
 		aux+= p[i];
 	}
 
@@ -88,10 +84,9 @@ int main(int argc, char* argv[]){
 	useconds = end.tv_usec - start.tv_usec;
 	milisecs = ((seconds) * 1000 + useconds/1000.0);
 	cout << n << "\t" << aux <<"\t"; 
-
-	pi = (l/aux)*(n);
-
+	
+	pi = 4*aux/n;
+	
 	cout << milisecs << "\t" << pi << endl;
-	return 0;
+	return 0; 
 }
-
---
+++ Reto_2/execute.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Command for compile c++ transp mat mul and run 
+# bash execute.sh compile run auto clean Sec_Transp Thread_Transp Fork_Transp
+compile='compile' # compile file 
+run='run' # run file 
+#if you use both can compile and run
+args=("$@")
+
+auto_exec(){
+	for i in {1..10}
+	do
+		for k in 1000 10000 100000 1000000 10000000
+			do
+			./"$1" $k
+			done
+	done
+}
+
+echo " ${args[0]} ${args[1]} ${args[2]} ${args[3]}"
+
+if [ "${args[0]}" == "$compile" ] &&  [ "${args[1]}" == "$run" ];
+then
+	echo "Ejecucion OMP NEEDLE 4 Threads"
+    echo "Intentos Aciertos Miliseg Pi"
+	g++ -o ${args[2]} -fopenmp ${args[2]}.cpp 
+	auto_exec ${args[2]}
+
+    echo "Ejecucion OMP DARTBOARD 4 Threads"
+    echo "Intentos Aciertos Miliseg Pi"
+	g++ -o ${args[3]} -fopenmp ${args[3]}.cpp 
+	auto_exec ${args[3]}
+
+else 
+	echo "revisa los comandos de entrada"
+
+fi
