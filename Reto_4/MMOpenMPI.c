2b3cc6afb933edbdcf0cc2084858476c9bb76af3
Angel David Santa Giraldo <angel.santa.0398@gmail.com>
2020-12-06 02:45:05 -0500

    Reto 4

--- Reto_4/MMOpenMPI.c
+++ Reto_4/MMOpenMPI.c
@@ -2,28 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 #include <mpi.h>
+#include <omp.h>
 
 //mpicc -fopenmp MMOpenMPI.c -o exec
 //mpirun -np 4 -hosts head,wn1,wn2,wn3 ./exec 8
 //mpirun -np 4 -machinefile mfile ./exec 8
 
-void writeTime(double tiempo, int tam, int wnodos){
-    FILE *f = fopen("timesP2PMPI.txt","a+");
-    fprintf(f,"%i;%i;%.6lf\n", wnodos, tam, tiempo);
-    fclose(f);
-}
+void writeTime(double tiempo, int tam, int wnodos);
 
-void printMat(double* mat, int n){
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = 0; j < n; j++)
-        {
-            printf("%.0f\t", mat[i*n+j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-}
+void printMat(double* mat, int n);
+
+void Matmul(int n, double* mat2, double* scatterMat, double* gatherMat);
 
 int main(int argc, char *argv[]){
     int n = atoi(argv[1]);
@@ -60,7 +49,7 @@ int main(int argc, char *argv[]){
 
     MPI_Scatter(&mat1[(n*n/numranks)*rank], n*n/numranks, MPI_DOUBLE, scatterMat, n*n/numranks, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     MPI_Barrier(MPI_COMM_WORLD);
-
+    /*
     int sum = 0;
 
     for(int fil = 0; fil < n/numranks; fil++){
@@ -72,6 +61,8 @@ int main(int argc, char *argv[]){
             sum = 0;
         }
     }
+    */
+    Matmul(n, mat2, scatterMat, gatherMat);
 
     MPI_Gather(gatherMat, n*n/numranks, MPI_DOUBLE, &result[(n*n/numranks)*rank], n*n/numranks, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     MPI_Barrier(MPI_COMM_WORLD);
@@ -82,15 +73,48 @@ int main(int argc, char *argv[]){
         tiempo = endTime - startTime;
         printf("\ntiempo: %3f\n", tiempo);
         //writeTime(tiempo, n, numranks);
-        printf("A\n");
-        printMat(mat1, n);
-        printf("B\n");
-        printMat(mat2, n);
-        printf("Result\n");
-        printMat(result, n);
+        //printf("A\n");
+        //printMat(mat1, n);
+        //printf("B\n");
+        //printMat(mat2, n);
+        //printf("Result\n");
+        //printMat(result, n);
 	}
 
     MPI_Finalize();
 
     return 0;
+}
+
+
+void printMat(double* mat, int n){
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            printf("%.0f\t", mat[i*n+j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void writeTime(double tiempo, int tam, int wnodos){
+    FILE *f = fopen("timesP2PMPI.txt","a+");
+    fprintf(f,"%i;%i;%.6lf\n", wnodos, tam, tiempo);
+    fclose(f);
+}
+
+void Matmul(int n, double* mat2, double* scatterMat, double* gatherMat){
+    int sum = 0;
+
+    for(int fil = 0; fil < n/numranks; fil++){
+        for(int col = 0; col < n; col++){
+            for(int k = 0; k < n; k++){
+                sum = sum + mat2[n*k+col]*scatterMat[n*fil+k];
+            }
+            gatherMat[n*fil+col] = sum;
+            sum = 0;
+        }
+    }
 }
